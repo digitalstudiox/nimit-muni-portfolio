a053473a87a6992b14dfdd4e9de6b85d66390871
Pankti Muni <munipankti@gmail.com>
2026-01-06 12:23:41 +0530

    Configure Vite for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/nimit-muni-portfolio/",
   server: {
     host: "::",
     port: 8080,
