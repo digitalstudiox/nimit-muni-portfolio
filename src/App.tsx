a386f814d79e3ace079204e8e95685429213ef41
Pankti Muni <munipankti@gmail.com>
2026-01-06 13:18:45 +0530

    Fix GitHub Pages routing using HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
@@ -26,12 +26,12 @@ const App = () => {
         <Toaster />
         <Sonner />
         {/* 👇 IMPORTANT: basename must match repo name */}
-        <BrowserRouter basename="/nimit-muni-portfolio">
+        <HashRouter basename="/nimit-muni-portfolio">
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </TooltipProvider>
     </QueryClientProvider>
   );
